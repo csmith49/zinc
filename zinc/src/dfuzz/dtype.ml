7dd13df4614884beae4d5d69141d11357d91b064
Calvin Smith <cjsmith@cs.wisc.edu>
2017-09-21 17:44:56 -0500

    split instantiate into two versions, one per variable type in dtypes

--- zinc/src/dfuzz/dtype.ml
+++ zinc/src/dfuzz/dtype.ml
@@ -41,42 +41,40 @@ let rec name_to (n : Name.t) (db : int) (dt : t) : t = match dt with
   | Tensor (l, r) -> Tensor (name_to n db l, name_to n db r)
   | _ -> dt (* catches Base and Bound case *)
 
-(*
 let rec replace (img : t) (db : int) (dt : t) : t = match dt with
-  | Bound i -> if i == db then img else dt
+  | Bound i -> if i = db then img else dt
   | Precise p -> begin match p with
-      | N s ->
-        let s' = begin match img with
-          | Payload (PSize size) -> Size.replace size db s
-          | _ -> s
-        end in Precise (N s')
-      | M (s, dt') ->
-        let s' = begin match img with
-          | Payload (PSize size) -> Size.replace size db s
-          | _ -> s
-        end in Precise (M (s', replace img db dt'))
-      | R s ->
-        let s' = begin match img with
-          | Payload (PSens sens) -> Sensitivity.replace sens db s
-          | _ -> s
-        end in Precise (R s')
+      | M (s, dt') -> Precise (M (s, replace img db dt'))
+      | _ -> Precise p
     end
   | Quant (q, k, Sc body) -> Quant (q, k, Sc (replace img (db + 1) body))
   | Func (m, codom) -> begin match m with
-      | Modal (s, dom) ->
-        let s' = begin match img with
-          | Payload (PSens sens) -> Sensitivity.replace sens db s
-          | _ -> s
-        end in Func (Modal (s', replace img db dom), replace img db codom)
+      | Modal (s, dom) -> Func (Modal (s, replace img db dom), replace img db codom)
     end
   | Tensor (l, r) -> Tensor (replace img db l, replace img db r)
-  | Payload _ -> failwith "can't replace payload in transit"
-  | _ -> dt (* catches base and bound case *)
+  | _ -> dt
 
-(* the payoff *)
-let abstract (n : Name.t) (dt : t) : scope = Sc (name_to n 0 dt)
-let instantiate (img: t) (s : scope) : t = match s with
+let rec replace_sensitivity (img : Sensitivity.t) (db : int) (dt : t) : t = match dt with
+  | Precise p -> begin match p with
+      | N s -> Precise (N (Sensitivity.replace img db s))
+      | M (s, dt') -> Precise (M (Sensitivity.replace img db s, dt'))
+      | R s -> Precise (R (Sensitivity.replace img db s))
+    end
+  | Quant (q, k, Sc body) -> Quant (q, k, Sc (replace_sensitivity img (db + 1) body))
+  | Func (m, codom) -> begin match m with
+      | Modal (s, dom) ->
+        let dom' = replace_sensitivity img db dom in
+        let codom' = replace_sensitivity img db codom in
+        Func (Modal (Sensitivity.replace img db s, dom'), codom')
+    end
+  | Tensor (l, r) -> Tensor (replace_sensitivity img db l, replace_sensitivity img db r)
+  | _ -> dt
+
+let instantiate (img : t) (s : scope) : t = match s with
   | Sc body -> replace img 0 body
+
+let instantiate_sensitivity (img : Sensitivity.t) (s : scope) : t = match s with
+  | Sc body -> replace_sensitivity img 0 body
 
 (* submodules will want to refer to this type *)
 type dtype = t
@@ -87,10 +85,4 @@ module Prefix = struct
   type binding = Name.t * quantifier * kind
   (* so a prefix maintains a stack of bindings *)
   type t = binding Stack.t
-  (* infix binding application/inverses *)
-  let (@>) (b : binding) (dt : dtype) : dtype = match b with
-    | (n, q, k) -> Quant (q, k, abstract n dt)
-  let (<@) (n : Name.t) (dt : dtype) : (binding * dtype) option = match dt with
-    | Quant (q, k, body) -> Some ((n, q, k), instantiate (Free n) body)
-    | _ -> None
-end *)
+end
