c207b1cfb4979004a8329870d80e60e9a7712dd1
Calvin Smith <cjsmith@cs.wisc.edu>
2017-09-21 19:18:06 -0500

    cleanup, initializing constraint building wrt new contexts and variable namings

--- zinc/src/dfuzz/constraint.ml
+++ zinc/src/dfuzz/constraint.ml
@@ -1,10 +1,14 @@
-(* constraints only have two forms - equality amongst size variables and relations amonst sensitivities *)
+open Stack.Alt
+
+(* constraints have two forms - equality, and inequality *)
 type relation =
-  | EQ of Size.t * Size.t
-  | LEQ of Sensitivity.t * Sensitivity.t
+  | Eq of Sensitivity.t * Sensitivity.t
+  | LEq of Sensitivity.t * Sensitivity.t
 
 (* but we maintain a list of these relations as we abduce *)
 type t = relation Stack.t
+
+(* an important conversion is contexts to constraints *)
 
 (* and at some point we need to convert each to a z3 formula *)
 (*
@@ -14,6 +18,3 @@ let rec to_z3 : t -> z3 = function
     let r' = relation_to_z3 r in
     z3.and r' (to_z3 rs)
 *)
-
-(* this lets us define the abduction procedure *)
-let rec abduction (l : Dtype.t) (r : Dtype.t) : t option =
