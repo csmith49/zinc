aa5727af3b45be9b08c7aa1d23dfeaa419a6472e
Calvin Smith <cjsmith@cs.wisc.edu>
2017-11-03 12:47:03 -0500

    updated printing

--- zinc/src/zinc.ml
+++ zinc/src/zinc.ml
@@ -3,10 +3,16 @@ open Inference
 
 (* references for command line arguments *)
 let benchmark_name = ref ""
+let verbosity = ref 1
+
+(* functions defining the level of printing *)
+let normal_print : string -> unit = fun s -> if !verbosity >= 1 then print_string s else ()
+let bm_print : string -> unit = fun s -> if !verbosity >= 3 then print_string s else ()
 
 (* the actual command line arguments *)
 let spec_list = [
   ("-bm", Arg.Set_string benchmark_name, "Sets the benchmark to test");
+  ("-v", Arg.Set_int verbosity, "Sets the level of verbosity (>= 3 for benchmarking output)");
 ]
 
 (* populate the references - no anonymous functions *)
@@ -14,12 +20,12 @@ let usage_message = ""
 let anon_function = fun s -> ()
 let _ = Arg.parse (Arg.align spec_list) anon_function usage_message
 
-let _ = print_endline ("Evaluating benchmark: " ^ !benchmark_name)
+let _ = normal_print ("Evaluating benchmark: " ^ !benchmark_name ^ "\n")
 
 (* main loop happens here *)
 let rec extract_benchmark (name : string) (bs : Benchmark.t list) : Benchmark.t = match bs with
   | b :: bs' -> if b.Benchmark.name = name then b else extract_benchmark name bs'
-  | _ -> failwith "can't find provided benchmark"
+  | _ -> failwith ("can't find provided benchmark: " ^ name)
 
 (* pull the benchmark from the arguments *)
 let benchmark = extract_benchmark !benchmark_name Benchmark.basic
@@ -41,6 +47,7 @@ open Name.Alt
 
 (* the infinite loop, in a function we can back out of *)
 let synthesize (bm : Benchmark.t) : unit =
+
   (* generate start node and initialize frontier *)
   let start_node = Benchmark.to_node bm in
   let _ = frontier := Frontier.push (Node.to_priority start_node) start_node !frontier in
@@ -55,8 +62,7 @@ let synthesize (bm : Benchmark.t) : unit =
     let _ = counter := !counter + 1 in
 
     (* PRINTING *)
-    let _ = print_endline ("Checking: " ^ (Fterm.to_string tm)) in
-    let _ = print_endline ("    Obligation: " ^ (Constraint.to_string node.Node.obligation)) in
+    let _ = normal_print ("Checking: " ^ (Fterm.to_string tm) ^ "\n    Obligation: " ^ (Constraint.to_string node.Node.obligation) ^ "\n") in
     
     (* check if the obligation is satisfiable *)
     let meets_obligation = Strategy.check node.Node.obligation in
@@ -65,7 +71,7 @@ let synthesize (bm : Benchmark.t) : unit =
     if meets_obligation then
 
       (* PRINTING *)
-      let _ = print_endline ("    Satisfiable!") in
+      let _ = normal_print ("    Satisfiable!\n") in
 
       (* check if tm is a solution *)
       if (Fterm.wild_closed tm) then
@@ -81,17 +87,14 @@ let synthesize (bm : Benchmark.t) : unit =
         let f = fun p -> Subproblem.specialize root p subproblem.Subproblem.context in
         let solutions = CCList.flat_map f proposals in
         let steps = CCList.filter_map (fun p -> 
-          let _ = print_string ("\tEx: " ^ (Proposal.to_string p) ^ "...") in
-          let _ = print_string ((Constraint.to_string p.Proposal.obligation) ^ "...") in
           let ans = Subproblem.insert_proposal p subproblem in
-          let _ = print_endline (if CCOpt.is_some ans then "ok" else "no")
-          in ans) 
+          let _ = normal_print ("\tEx: " ^ (Proposal.to_string p) ^ "..." ^ (Constraint.to_string p.Proposal.obligation) ^ "..." ^ (if CCOpt.is_some ans then "ok" else "no") ^ "\n") in ans)
           (solutions @ (CCOpt.to_list (Subproblem.lambda_proposal subproblem))) in
         
         CCList.iter (fun n -> 
           frontier := Frontier.push (Node.to_priority n) n !frontier) steps
     else
-      print_endline ("    Unsatisfiable.")
+      normal_print ("    Unsatisfiable.\n")
   done;;
 
 (* run the experiment, and catch the output *)
