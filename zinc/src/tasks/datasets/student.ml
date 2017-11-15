57b7f76d5c42733900bc27422df7ed786ae5340d
Calvin Smith <cjsmith@cs.wisc.edu>
2017-11-15 09:33:27 -0600

    final benchmarks up and running

--- zinc/src/tasks/datasets/student.ml
+++ zinc/src/tasks/datasets/student.ml
@@ -37,6 +37,8 @@ let student_02 = {
 }
 
 (* average weekend alcohol consumption per address type *)
+(* λx:712711699.λy:712711699.group_map (group_map (partition (x:712711699) (address_type) (y:712711699)) (map (λz:712711699.wknd_to_val (weekend_consumption (z:712711699))))) (average) *)
+
 let student_03 = {
   name = "student_03";
   mechanism = Partition (
@@ -99,19 +101,17 @@ let student_06 = {
   name = "student_06";
   mechanism = Partition (
     Signature.Student.reason_t,
-    [Value.Discrete "proximity"; Value.Discrete "reputation"; Value.Discrete "other"]);
+    [Value.Discrete "reputation"; Value.Discrete "proximity"]);
   budget = 100;
   grammar = student_sig @ Signature.Database.signature;
   examples = [
     (Value.Bag [
-      Signature.Student.make 10 "reputation" 5 5 "rural" false 3 10;
+      Signature.Student.make 20 "reputation" 0 0 "rural" true 2 10;
       Signature.Student.make 20 "reputation" 0 0 "rural" true 2 1;
       Signature.Student.make 17 "proximity" 4 3 "urban" true 5 3;
-      Signature.Student.make 3 "other" 3 4 "urban" false 0 2
     ], Value.Bag [
       Value.Pair (Value.Discrete "reputation", Value.Real 11.0);
       Value.Pair (Value.Discrete "proximity", Value.Real 3.0);
-      Value.Pair (Value.Discrete "other", Value.Real 2.0);
     ])
   ];
 }
@@ -120,7 +120,7 @@ let student_06 = {
 let student_07 = {
   name = "student_07";
   mechanism = Exponential (
-    Signature.Student.family_t, 
+    Signature.Student.address_type_t, 
     [Value.Discrete "rural"; Value.Discrete "urban";]);
   budget = 5;
   grammar = student_sig @ Signature.Database.signature;
@@ -130,13 +130,11 @@ let student_07 = {
       Signature.Student.make 20 "reputation" 0 0 "rural" true 2 0;
       Signature.Student.make 9 "proximity" 4 3 "urban" true 0 3;
     ], Value.Discrete "rural");
-    (* (Value.Bag [
-      Signature.Student.make 10 "reputation" 5 5 "rural" false 3 10;
-      Signature.Student.make 18 "reputation" 0 0 "rural" true 2 0;
+    (Value.Bag [
+      Signature.Student.make 10 "reputation" 5 5 "rural" false 7 10;
+      Signature.Student.make 18 "reputation" 0 0 "rural" true 7 0;
       Signature.Student.make 20 "proximity" 4 3 "urban" true 1 3;
-      Signature.Student.make 20 "proximity" 4 3 "urban" true 1 3;
-      Signature.Student.make 20 "proximity" 4 3 "urban" true 1 3;
-      ], Value.Discrete "urban") *)
+      ], Value.Discrete "urban")
   ]
 }
 
